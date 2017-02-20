e2d2d7fb5f0a12f041eeef2d461aa4ec3a553a55
wiheto <whthompson@ymail.com>
2017-02-20 15:19:02 +0100

    small fix to setup

--- setup.py
+++ setup.py
@@ -4,10 +4,11 @@ setup(name='fetchopenfmri',
       description='Fetches files for the latest version of an openfMRI dataset',
       packages=find_packages(),
       author='William Hedley Thompson',
+      author_email='hedley@startmail.com',
       url='https://www.github.com/wiheto/fetchopenfmri',
       entry_points={
         'console_scripts': ['fetchopenfmri=fetchopenfmri.fetch:main'],
       },
-      download_url = 'https://github.com/wiheto/fetchopenfmri/archive/v1.tar.gz'
-      keyworkds={'openfmri','open data','fmri'}
+      download_url = 'https://github.com/wiheto/fetchopenfmri/archive/v1.tar.gz',
+      keywords=['openfmri','open data','fmri']
       )
